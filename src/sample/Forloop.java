69a99db0dcfdb0c3a669735466fa139ef90c614f
Shiva@123 <poovazhaki@rsninfotech.com>
2019-04-27 19:04:45 +0530

    Sample Code added

---
+++ src/sample/Forloop.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class Forloop {
+	public static int play() {
+        String input;int result;
+        do {
+        input = ("Do you want to play-Enter 1 if yes , 0 if no");
+        result = Integer.parseInt(input);
+        } while (result != 1 || result != 0);
+        return result;
+    }
+
+}
